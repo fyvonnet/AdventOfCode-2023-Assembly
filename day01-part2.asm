a4c5ef72b230c56feadfec139a228998586f456e
Franck YVONNET <franck@yvonnet.fr>
2023-12-01 13:31:09 +0100

    Read second digit from the end of the line

--- day01-part2.asm
+++ day01-part2.asm
@@ -20,20 +20,29 @@ main:
 	clr	s0
 
 loop_input:
+	# read first digit fromp the beginning of the line
+	li	a1, 1				# move forward
 	call	read_next_digit
 	li	t0, 10
 	mul	s1, t0, a1
-	mv	s2, a1				# first digit is also last digit for now
-loop_line:
+
+	# move pointer to the end of line
+	li	t1, ASCII_LF
+	dec	a0
+loop:
+	inc	a0
+	lb	t0, 0(a0)
+	bne	t0, t1, loop
+
+	addi	s3, a0, 1			# save pointer to begining of next line
+	addi	a0, a0, -1			# move input pointer to last character of the line
+	
+	# read second digit from  the end of the line
+	li	a1, -1				# move backward
 	call    read_next_digit
-	li	t0, -1
-	beq	a1, t0, loop_line_end		# last digit is -1, EOL reached
-	mv	s2, a1				# save last read digit
-	j	loop_line			
-loop_line_end:
-	add	s1, s1, s2			# add last digit read before -1 to the calibration value
-stop_here:
+	add	s1, s1, a1			# add second digit to the calibration value
 	add	s0, s0, s1			# add the calibration value to the sum
+	mv	a0, s3
 	blt	a0, s11, loop_input		# loop of EOF not reached
 
 	mv	a0, s0
@@ -44,33 +53,35 @@ stop_here:
 	ecall
 
 read_next_digit:
-	addi	sp, sp, -24
+	addi	sp, sp, -32
 	sd	ra,  0(sp)
 	sd	s0,  8(sp)
 	sd	s1, 16(sp)
+	sd	s2, 24(sp)
 	
-	addi	s1, a0, -1
+	mv	s2, a1
+	sub	t0, zero, a1
+	add	s1, a0, t0			# pre-move pointer in reverse direction
 rnd_loop:
-	inc	s1
+	add	s1, s1, s2			# move input pointer
 	lb	s0, 0(s1)			# read character
-	li	t0, ASCII_LF
-	beq	s0, t0, rnd_eol			# EOL reached
 	mv	a0, s0
 	call	is_digit			# check if digit
 	beqz	a0, rnd_ldigit			# not a digit, attemps reading as a letter-digit
 	li	t0, ASCII_ZERO
 	sub	a1, s0, t0			# turn ASCII code to actual value
-	j	rnd_end
-rnd_eol:
-	li	a1, -1				# eol reached, no new digit
 rnd_end:
-	inc	s1				# increase input pointer
+	add	s1, s1, s2			# move input pointer
 	mv	a0, s1				# copy pointer back to a0
+
 	ld	ra,  0(sp)
 	ld	s0,  8(sp)
 	ld	s1, 16(sp)
-	addi	sp, sp, 24
+	ld	s2, 24(sp)
+	addi	sp, sp, 32
 	ret
+
+
 rnd_ldigit:
 	li	t0, 0				# L
 	li	t2, 8				# R
@@ -91,15 +102,14 @@ rnd_ldigit_loop:
 	mv	a0, s1				# pointer to cadidate string
 	ld	a1, 3(s6)			# pointer to model string
 	call	validate
-	#li	a1, -1
 	beqz	a0, rnd_loop			# not validated, keep on searching
 	lb	a1, 2(s6)			# value of the ldigit
 	j	rnd_end
 search_right:
 	addi	t0, t1, 1
-	j	rnd_ldigit_loop
+	j	rnd_ldigit_loop			# L = m + 1
 search_left:
-	addi	t2, t1, -1
+	addi	t2, t1, -1			# R = m - 1
 	j	rnd_ldigit_loop
 	
 is_digit:
