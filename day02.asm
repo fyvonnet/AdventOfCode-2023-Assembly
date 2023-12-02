0de54bc897f95d12a7f3512e10c9de6e0166af68
Franck YVONNET <franck@yvonnet.fr>
2023-12-02 12:44:29 +0100

    some refactoring

--- day02.asm
+++ day02.asm
@@ -10,17 +10,20 @@ main:
 	call    map_input_file
 	add	s11, a0, a1
 
-	li	s1, 12				# red
-	li	s2, 13				# green
-	li	s3, 14				# blue
+	la	s1, color_skip
+	la	s2, bag_content
+
+	add	sp, sp, -3			# allocate stack space for maxima for each color
+	mv	s3, sp
 
 	clr	s4				# initialize sum for part 1
 	clr	s5				# initialize sum for part 2
 
 loop_input:
-	clr	s6				# max red
-	clr	s7				# max green
-	clr	s8				# max blue
+	# set maxima to zero
+	sb	zero, 0(s3)
+	sb	zero, 1(s3)
+	sb	zero, 2(s3)
 	li	s9, 1				# mark line as OK
 	bge	a0, s11, loop_input_end
 	addi	a0, a0, 5			# skip "Game "
@@ -31,12 +34,12 @@ loop_line:
 	call	parse_integer
 	inc	a0				# skip to color name
 	lb	t1, 0(a0)
-	li	t0, ASCII_B
-	beq	t0, t1, blue
-	li	t0, ASCII_G
-	beq	t0, t1, green
 	li	t0, ASCII_R
 	beq	t0, t1, red
+	li	t0, ASCII_G
+	beq	t0, t1, green
+	li	t0, ASCII_B
+	beq	t0, t1, blue
 back:
 	li	t0, ASCII_LF
 	lb	t1, 0(a0)
@@ -47,12 +50,16 @@ loop_line_end:
 	beqz	s9, skip_add
 	add	s4, s4, s0			# add game number to the part 1 sum
 skip_add:
-	mul	s6, s6, s7
-	mul	s6, s6, s8
+	li	s6, 1
+	mv	t0, s3
+	.rept 3
+	lb	t1, 0(t0)
+	mul	s6, s6, t1
+	inc	t0
+	.endr
 	add	s5, s5, s6			# add power to the part 2 sum
 	inc	a0
 	j	loop_input
-	
 
 loop_input_end:
 
@@ -66,48 +73,41 @@ loop_input_end:
 	li      a0, EXIT_SUCCESS
 	ecall
 
-blue:
-	addi	a0, a0, 4
-	ble	a1, s3, blue_ok
-	li	s9, 0
-blue_ok:
-	ble	a1, s8, skip_blue_max
-	mv	s8, a1
-skip_blue_max:
-	j	back
-
-green:
-	addi	a0, a0, 5
-	ble	a1, s2, green_ok
-	li	s9, 0
-green_ok:
-	ble	a1, s7, skip_green_max
-	mv	s7, a1
-skip_green_max:
-	j	back
-
 red:
-	addi	a0, a0, 3
-	ble	a1, s1, red_ok
-	li	s9, 0
-red_ok:
-	ble	a1, s6, skip_red_max
-	mv	s6, a1
-skip_red_max:
-	j	back
+	li	t0, 0
+	j	next
+green:
+	li	t0, 1
+	j	next
+blue:
+	li	t0, 2
+next:
+	# load chracters offset
+	add	t1, s1, t0
+	lb	t1, 0(t1)
+	add	a0, a0, t1
 
-skip_to_next_line:
-	li	t1, ASCII_LF
-skip_to_next_line_loop:
-	lb	t0, 0(a0)
-	beq	t0, t1, skip_to_next_line_end
-	inc	a0
-	j	skip_to_next_line_loop
-skip_to_next_line_end:
-	inc	a0
-	ret
+	# check if count OK
+	add	t1, s2, t0
+	lb	t1, 0(t1)
+	ble	a1, t1, ok
+	li	s9, 0				# mark line as not valid
+ok:
+	# check if new maximum found
+	add	t1, s3, t0
+	lb	t2, 0(t1)
+	ble	a1, t2, skip_max
+	sb	a1, 0(t1)
+skip_max:
+	j	back
 
 	.section .rodata
+
+bag_content:
+	.byte	12, 13, 14
+
+color_skip:
+	.byte	3, 5, 4
 
 filename:
 	.string "inputs/day02"
