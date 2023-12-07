3d33d6339b8ac17d59c587679abb0122caeb0f48
Franck YVONNET <franck@yvonnet.fr>
2023-12-07 10:38:47 +0100

    first commit

---
+++ day07.asm
@@ -0,0 +1,214 @@
+	.global main
+
+	.include "macros.inc"
+	.include "constants.inc"
+
+	.section .text
+
+main:
+	la      a0, filename
+	call    map_input_file
+	mv	s0, a0
+	add	s11, a0, a1
+
+	clr	s1				# initialize hands counter
+loop_read_hands:
+	inc	s1
+	addi	sp, sp, -10
+	mv	s0, a0
+	call	decode_cards
+	sd	a0, 0(sp)
+	addi	a0, s0, 6			# skip to bid
+	call	parse_integer
+	sh	a1, 8(sp)
+	inc	a0				# skip lf
+	blt	a0, s11, loop_read_hands
+
+	# sort hands
+	mv	a0, sp
+	mv	a1, s1
+	li	a2, 10
+	la	a3, compar_hands
+	call	quicksort
+	
+	li	s2, 1				# initialize rank
+	clr	s3				# initialize total winnings
+loop_winnings:
+	lh	t0, 8(sp)			# load bid
+	mul	t0, t0, s2			# multiply bid by rank
+	add	s3, s3, t0			# add winning to sum
+	addi	sp, sp, 10			# move hands pointer
+	inc	s2				# increment rank
+	dec	s1				# decrement counter
+	bnez	s1, loop_winnings
+
+	mv	a0, s3
+	call	print_int
+	
+	li      a7, SYS_EXIT
+	li      a0, EXIT_SUCCESS
+	ecall
+
+	# a0: input pointer
+decode_cards:
+	add	sp, sp, -16
+	sd	ra,  0(sp)
+	sd	s0,  8(sp)
+
+	addi	sp, sp, -8			# allocate 64 bits
+	sh	zero, 0(sp)			# start with 16 bits of zeros
+
+	# load cards in the last 5 bytes
+	addi	a1, sp, 3
+	call	read_cards
+
+	# store the hand strength in the 3rd byte
+	addi	a0, sp, 3
+	call	get_hand_strength
+	sb	a0, 2(sp)
+
+	# invert bytes order
+	mv	t0, sp
+	addi	t1, sp, 7
+	.rept	4
+	lb	t2, 0(t0)
+	lb	t3, 0(t1)
+	sb	t2, 0(t1)
+	sb	t3, 0(t0)
+	inc	t0
+	dec	t1
+	.endr
+
+	ld	a0, 0(sp)			# load final 64-bits value
+	addi	sp, sp, 8			# free stack space
+
+	ld	ra,  0(sp)
+	ld	s0,  8(sp)
+	add	sp, sp, 16
+	ret
+	
+
+
+	# a0: cards vector pointer
+get_hand_strength:
+	add	sp, sp, -16
+	sd	ra,  0(sp)
+	sd	s0,  8(sp)
+
+	mv	s0, a0
+	addi	sp, sp, -13
+	sd	zero, 0(sp)
+	sw	zero, 8(sp)
+	sb	zero, 12(sp)
+	li	t0, 5
+loop_count_cards:
+	lb	t1, 0(s0)			# load card strength
+	add	t1, t1, sp			# compute counter address
+	lb	t2, 0(t1)			# load count
+	inc	t2				# increment count
+	sb	t2, 0(t1)			# store count
+	inc	s0
+	dec	t0
+	bnez	t0, loop_count_cards
+
+	# sort count in reverse to generate a 64-bits value
+	mv	a0, sp
+	li	a1, 13
+	li	a2, 1
+	la	a3, compar
+	call	quicksort
+
+	# search hand 64-bits value in the hand types vector
+	ld	t0, 0(sp)
+	la	t1, hand_types
+	mv	t3, t1
+loop_search_type:
+	ld	t2, 0(t1)
+	beq	t0, t2, loop_search_type_end
+	addi	t1, t1, 8
+	j	loop_search_type
+loop_search_type_end:
+	sub	t1, t1, t3
+	srli	a0, t1, 3
+
+	addi	sp, sp, 13
+
+	ld	ra,  0(sp)
+	ld	s0,  8(sp)
+	add	sp, sp, 16
+	ret
+	
+
+
+	# a0: input pointer
+	# a1: destination pointer
+read_cards:
+	addi	sp, sp, -32
+	sd	ra,  0(sp)
+	sd	s0,  8(sp)
+	sd	s1, 16(sp)
+	sd	s2, 24(sp)
+
+	mv	s0, a0
+	mv	s1, a1
+	li	s2, 5
+loop_read_cards:
+	lb	a0, 0(s0)
+	call	get_card_num
+	sb	a0, 0(s1)
+	inc	s0
+	inc	s1
+	dec	s2
+	bnez	s2, loop_read_cards
+
+	ld	ra,  0(sp)
+	ld	s0,  8(sp)
+	ld	s1, 16(sp)
+	ld	s2, 24(sp)
+	addi	sp, sp, 32
+	ret
+
+
+	# a0: ascii code of card
+	# return: card strength
+get_card_num:
+	la	t0, cards
+	mv	t2, t0
+loop_get_card_num:
+	lb	t1, 0(t0)
+	beq	t1, a0, get_card_num_end
+	inc	t0
+	j	loop_get_card_num
+get_card_num_end:
+	sub	a0, t0, t2
+	ret
+
+compar:
+	lb	a0, 0(a0)
+	lb	a1, 0(a1)
+	sub	a0, a1, a0
+	ret
+
+compar_hands:
+	ld	a0, 0(a0)
+	ld	a1, 0(a1)
+	sub	a0, a0, a1
+	ret
+
+	.section .rodata
+
+filename:
+	.string "inputs/day07"
+	#.string "inputs/day07-test"
+
+cards:
+	.ascii	"23456789TJQKA"
+
+hand_types:
+	.quad	0x0000000101010101			# high card
+	.quad	0x0000000001010102			# one pari
+	.quad	0x0000000000010202			# two pairs
+	.quad	0x0000000000010103			# Three of a kind
+	.quad	0x0000000000000203			# full house
+	.quad	0x0000000000000104			# four of a kind
+	.quad	0x0000000000000005			# Five of a kind
