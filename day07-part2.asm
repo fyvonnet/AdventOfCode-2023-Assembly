1a1945068e15229f378b60eac90d775cbad50015
Franck YVONNET <franck@yvonnet.fr>
2024-09-30 07:02:19 +0200

    some changes

--- day07-part2.asm
+++ day07-part2.asm
@@ -14,25 +14,34 @@ main:
 	clr	s1				# initialize hands counter
 loop_read_hands:
 	inc	s1
-	addi	sp, sp, -10			# allocate hand space in the stack
-	mv	a1, sp
-	call	decode_hand
+	addi	sp, sp, -6			# allocate hand space in the stack
+	#mv	a1, sp
+	#call	decode_hand
+	mv	s0, a0
+	call	read_cards
+	sw	a0, 0(sp)
+	add	a0, s0, 6			# skip to bid
+	call	parse_integer
+	sh	a1, 4(sp)
+	inc	a0				# skip LF
+end:
 	blt	a0, s11, loop_read_hands
+
 
 	# sort hands
 	mv	a0, sp
 	mv	a1, s1
-	li	a2, 10
+	li	a2, 6
 	la	a3, compar_hands
 	call	quicksort
 	
 	li	s2, 1				# initialize rank
 	clr	s3				# initialize total winnings
 loop_winnings:
-	lh	t0, 8(sp)			# load bid
+	lh	t0, 4(sp)			# load bid
 	mul	t0, t0, s2			# multiply bid by rank
 	add	s3, s3, t0			# add winning to sum
-	addi	sp, sp, 10			# move hands pointer
+	addi	sp, sp, 6			# move hands pointer
 	inc	s2				# increment rank
 	dec	s1				# decrement counter
 	bnez	s1, loop_winnings
@@ -44,103 +53,65 @@ loop_winnings:
 	li      a0, EXIT_SUCCESS
 	ecall
 
-	# a0: input pointer
-	# a1: storage pointer
-decode_hand:
-	add	sp, sp, -24
-	sd	ra,  0(sp)
-	sd	s0,  8(sp)
-	sd	s1, 16(sp)
-
-	mv	s0, a0
-	mv	s1, a1
-
-	sh	zero, 0(s1)			# start with 16 bits of zeros
-
-	# load cards in the last 5 bytes
-	addi	a1, s1, 3
-	call	read_cards
-
-	# store the hand strength in the 3rd byte
-	addi	a0, s1, 3
-	call	get_hand_strength
-	sb	a0, 2(s1)
-
-	# invert bytes order
-	mv	t0, s1
-	addi	t1, s1, 7
-	.rept	4
-	lb	t2, 0(t0)
-	lb	t3, 0(t1)
-	sb	t2, 0(t1)
-	sb	t3, 0(t0)
-	inc	t0
-	dec	t1
-	.endr
-
-	addi	a0, s0, 6			# skip to bid
-	call	parse_integer
-	sh	a1, 8(s1)			# store bid
-	inc	a0				# skip lf
-
-	ld	ra,  0(sp)
-	ld	s0,  8(sp)
-	ld	s1, 16(sp)
-	add	sp, sp, 24
-	ret
-	
-
 
 	# a0: cards vector pointer
-get_hand_strength:
+hand_strength:
 	add	sp, sp, -16
 	sd	ra,  0(sp)
 	sd	s0,  8(sp)
 
-	mv	s0, a0
+	# allocate and zero the counters
 	addi	sp, sp, -13
 	sd	zero, 0(sp)
 	sw	zero, 8(sp)
 	sb	zero, 12(sp)
-	li	t0, 5
-loop_count_cards:
-	lb	t1, 0(s0)			# load card strength
-	add	t1, t1, sp			# compute counter address
-	lb	t2, 0(t1)			# load count
-	inc	t2				# increment count
-	sb	t2, 0(t1)			# store count
-	inc	s0
-	dec	t0
-	bnez	t0, loop_count_cards
 
-	lb	s0, 0(sp)			# get jokers count
-	sb	zero, 0(sp)			# remove jokers from the counts
+	# count the cards
+	.rept 5
+	andi	t0, a0, 0b1111			# get card value
+	add	t0, t0, sp			# counter address
+	lb	t1, 0(t0)			# load counter
+	inc	t1				# increment counter
+	sb	t1, 0(t0)			# save counter
+	srli	a0, a0, 4			# shift cards code
+	.endr
 
-	# sort count in reverse to generate a 64-bits value
+	lb	s0, 0(sp)			# load jokers count
+	sb	zero, 0(sp)			# zero jokers count
+
+	# sort counts
 	mv	a0, sp
 	li	a1, 13
 	li	a2, 1
-	la	a3, compar
+	la	a3, compar_counts
 	call	quicksort
 
-	lb	t0, 0(sp)			# load highest card count
-	add	t0, t0, s0			# add jokers
-	sb	t0, 0(sp)			# store new count
+	addi	sp, sp, 8 			# skip to the 5 last counts
 
-	# search hand 64-bits value in the hand types vector
-	ld	t0, 0(sp)
+	lb	t0, 4(sp)			# load highest cards count
+	add	t0, t0, s0			# add jokers
+	sb	t0, 4(sp)			# save highest cards count
+	
+	clr	t0				# initialize hand type code
+
+	# fill hand type code
+	.rept 5
+	slli	t0, t0, 4
+	lb	t1, 0(sp)
+	or	t0, t0, t1
+	addi	sp, sp, 1 
+	.endr
+
 	la	t1, hand_types
-	mv	t3, t1
+	mv	t2, t1
 loop_search_type:
-	ld	t2, 0(t1)
-	beq	t0, t2, loop_search_type_end
-	addi	t1, t1, 8
+	lw	t3, 0(t1)
+	beq	t3, t0, loop_search_type_end
+	addi	t1, t1, 4
 	j	loop_search_type
 loop_search_type_end:
-	sub	t1, t1, t3
-	srli	a0, t1, 3
-
-	addi	sp, sp, 13
+	sub	t1, t1, t2
+	srli	a0, t1, 2
 
 	ld	ra,  0(sp)
 	ld	s0,  8(sp)
@@ -151,30 +122,32 @@ loop_search_type_end:
 
 	# a0: input pointer
 	# a1: destination pointer
+	# return cards code
 read_cards:
-	addi	sp, sp, -32
+	addi	sp, sp, -24
 	sd	ra,  0(sp)
 	sd	s0,  8(sp)
 	sd	s1, 16(sp)
-	sd	s2, 24(sp)
 
 	mv	s0, a0
-	mv	s1, a1
-	li	s2, 5
-loop_read_cards:
-	lb	a0, 0(s0)
-	call	get_card_num
-	sb	a0, 0(s1)
-	inc	s0
-	inc	s1
-	dec	s2
-	bnez	s2, loop_read_cards
+	clr	s1				# initialize card code
+	.rept 5
+	slli	s1, s1, 4			# move code 4 bits to the left
+	lb	a0, 0(s0)			# load card character from the input
+	call	get_card_num			# get card index
+	or	s1, s1, a0			# add card index to the code
+	inc	s0				# increment input pointer
+	.endr
+	
+	mv	a0, s1
+	call	hand_strength
+	slli	a0, a0, 20			# shift hand strength left 5 * 4 bits
+	or	a0, a0, s1
 
 	ld	ra,  0(sp)
 	ld	s0,  8(sp)
 	ld	s1, 16(sp)
-	ld	s2, 24(sp)
-	addi	sp, sp, 32
+	addi	sp, sp, 24
 	ret
 
 
@@ -192,15 +165,15 @@ get_card_num_end:
 	sub	a0, t0, t2
 	ret
 
-compar:
+compar_counts:
 	lb	a0, 0(a0)
 	lb	a1, 0(a1)
-	sub	a0, a1, a0
+	sub	a0, a0, a1
 	ret
 
 compar_hands:
-	ld	a0, 0(a0)
-	ld	a1, 0(a1)
+	lw	a0, 0(a0)
+	lw	a1, 0(a1)
 	sub	a0, a0, a1
 	ret
 
@@ -208,15 +181,16 @@ compar_hands:
 
 filename:
 	.string "inputs/day07"
+	#.string "inputs/day07-test"
 
 cards:
 	.ascii	"J23456789TQKA"
 
 hand_types:
-	.quad	0x0000000101010101			# high card
-	.quad	0x0000000001010102			# one pari
-	.quad	0x0000000000010202			# two pairs
-	.quad	0x0000000000010103			# three of a kind
-	.quad	0x0000000000000203			# full house
-	.quad	0x0000000000000104			# four of a kind
-	.quad	0x0000000000000005			# five of a kind
+	.word	0x000011111			# high card
+	.word	0x000001112			# one pari
+	.word	0x000000122			# two pairs
+	.word	0x000000113			# three of a kind
+	.word	0x000000023			# full house
+	.word	0x000000014			# four of a kind
+	.word	0x000000005			# five of a kind
